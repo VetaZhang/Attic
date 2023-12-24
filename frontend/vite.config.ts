45d632c838fc4b82218876345046ba9a72df59b2
Veta <veta@MacBook-Pro.local>
2023-12-24 18:43:43 +0800

    add sass

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    // css预处理器
+    preprocessorOptions: {
+      scss: {
+        // 引入 mixin.scss 这样就可以在全局中使用 mixin.scss中预定义的变量了
+        // 给导入的路径最后加上 ; 
+        additionalData: '@import "@/assets/mixin.scss";'
+      }
+    }
+  }
 })
